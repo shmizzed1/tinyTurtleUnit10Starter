5fc3180c58f3d8e0a4542db322b84430af04cf71
Christian Cambizaca <christianismypapichulo@gmail.com>
2017-03-22 12:33:31 +0000

    lit

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
 TinyTurtle.apply(window, [undefined, 400, 400]);
 
-function square(){
-forward(50);
+function square(x){
+forward(x);
 right(90);
-forward(50);
+forward(x);
 right(90);
-forward(50);
+forward(x);
 right(90);
-forward(50);
+forward(x);
 right(90);
-forward(50);
+forward(x);
 }
 
+function triangle(x){
+    right(30);
+    forward(x);
+    right(120);
+    forward(x);
+    right(120);
+    forward(x);
+
+}
 // Type your function call below
 
+function house(x){
+square(x);
+triangle(x);
+}
 
-stamp();
+function rectanngle(len, wid){
+forward(len);
+right(90);
+forward(wid);
+right(90);
+forward(len);
+right(90);
+forward(wid);
+right(90);
+forward(len);
+}
+
+house(100);
+
